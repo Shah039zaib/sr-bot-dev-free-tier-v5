c21d3be72c843f1126e36198369234fb36ab0cff
Shah039zaib <125696406+Shah039zaib@users.noreply.github.com>
2025-11-30 21:26:18 +0500

    Update rate limiter configuration and options

--- src/middleware/rateLimiter.js
+++ src/middleware/rateLimiter.js
@@ -1,5 +1,10 @@
-const rateLimit = require('express-rate-limit');
-module.exports = rateLimit({
-  windowMs: 60*1000,
-  max: 120
+const rateLimit = require("express-rate-limit");
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,  // 15 minutes
+  max: 200,
+  standardHeaders: true,
+  legacyHeaders: false,
 });
+
+module.exports = limiter;
