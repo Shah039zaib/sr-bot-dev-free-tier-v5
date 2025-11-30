fdd0740da9dc65266d564e21a8e1bdb7e76da712
Shah039zaib <125696406+Shah039zaib@users.noreply.github.com>
2025-11-30 15:46:59 +0500

    Add files via upload

---
+++ src/middleware/errorHandler.js
@@ -0,0 +1,5 @@
+function errorHandler(err, req, res, next) {
+  console.error('ERR', err);
+  res.status(500).json({ error: 'Internal server error' });
+}
+module.exports = errorHandler;
---
+++ src/middleware/rateLimiter.js
@@ -0,0 +1,5 @@
+const rateLimit = require('express-rate-limit');
+module.exports = rateLimit({
+  windowMs: 60*1000,
+  max: 120
+});
